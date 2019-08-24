5d16349318349903e184ea4f9c6f7d96ef8509f7
Md Junaid Alam <mdjunaidalam5@gmail.com>
2019-08-24 17:37:58 +0530

    added demo folder and view source option

---
+++ demo/demo.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    $('.view-source').on('click', function(){
+        var cssClass = $(this).siblings('div[class*="sbl-"]')[0].className;
+        $.ajax({
+            url: '../dist/'+cssClass+'.css',
+            success: function(data){
+                console.log(data);
+                $('#cssSnippet').val(data);
+                $('#htmlSnippet').val($('.'+cssClass)[0].outerHTML)
+                $('.source-modal').show();
+            },
+            error: function(e){
+                console.log(e);
+            },
+        });
+    });
+
+    $('.source-modal').on('click', function(){
+        $(this).hide();
+    });
+    
+    $('.source-container').on('click', function(e){
+        e.stopPropagation(); 
+    })
+});
